7d4ac890df0c567dc166be4eba33c63c5afe7b4a
able <munirammar0@gmail.com>
2024-05-05 01:28:19 +0500

    Add Frontend UI.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from .models import MyChat
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+User = get_user_model()
+
+
+@login_required()
+def index(request):
+    frnd_name = request.GET.get('user')
+    mychat_data = None
+    if frnd_name:
+        if User.objects.filter(username=frnd_name).exists() and MyChat.objects.filter(me=request.user,frnd=User.objects.get(username=frnd_name)):
+            frnd =  User.objects.get(username=frnd_name)
+            mychat_data = MyChat.objects.filter(me=request.user,frnd=frnd)
+    frnds = User.objects.exclude(id=request.user.id)
+    return render(request,'index.html',{'my':mychat_data,'chats':mychat_data,'frnds':frnds})
+
